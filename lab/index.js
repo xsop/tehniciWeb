c45abe2e721bb58705f3a81c540934363daec4c5
xsop <40323764+xsop@users.noreply.github.com>
2023-03-20 09:53:48 +0200

    lab5

--- lab/index.js
+++ lab/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
+
 const app = express();
 console.log("Folder proiect: " + __dirname);
 
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(__dirname + "/resurse"));
+
 app.get("/ceva", function(req, res) {
-    res.send("altceva");
+    console.log("cale: " + req.url);
+    res.send("altceva" + req.ip);
 })
 
-app.get("/index.html", function(req, res) {
-    res.sendFile(__dirname + "/index2.html");
+
+app.get(["/index","/","/home"], function(req, res) {
+    res.render("pagini/index", {ip: req.ip, a: 10, b: 20});
 })
 
-app.listen(3000);
-console.log("Serverul a pornit pe portul 3000");
+app.get("/despre", function(req, res) {
+    res.render("pagini/despre");
+})
+app.listen(8080);
+console.log("Serverul a pornit pe portul 8080");
 
